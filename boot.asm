e6f5f56e1966b8fd1abdc25ff3a8afd219855969
stphnVi <stephanie1117@estudiantec.cr>
2024-09-01 13:43:36 -0600

    reading keys properly (structure) [add]

--- boot.asm
+++ boot.asm
@@ -14,8 +14,9 @@ mov dh, [row]
 mov dl, [column]                          
 int 0x10
 
-; Imprimir el nombre
+; Print name
 print_name:
+    
     mov ah, 0x0e    
     mov al, [si]    
     int 0x10        ; Call BIOS to print
@@ -23,7 +24,7 @@ print_name:
     cmp byte [si], 0 ; check final string
     jne print_name  
 
-jmp $
+jmp wait_key
 
 ;random rows & columns
 random:
@@ -46,6 +47,55 @@ random:
 
     ret
 
+wait_key:
+    mov ah, 0x00    ; Read key
+    int 0x16        ; Call BIOS to read the key
+    cmp al, 'j'     ; compare
+    je rotate_left
+    cmp al, 'k'
+    je rotate_down
+    cmp al, 'l'
+    je rotate_right
+    cmp al, 'i'
+    je rotate_up
+    jmp wait_key      ; check again for keys
+
+rotate_right:
+
+    mov ah, 0x0e    ; print caracteres
+    mov al, 'l'     ; load key value tecla
+    int 0x10        ; Call bios
+    jmp wait_key
+
+rotate_up:
+
+    mov ah, 0x0e    
+    mov al, 'i'   
+    int 0x10
+    jmp wait_key
+
+rotate_down:
+
+    mov ah, 0x0e    
+    mov al, 'k'   
+    int 0x10
+    jmp wait_key
+
+rotate_left:
+
+    mov ah, 0x0e    
+    mov al, 'j'  
+    int 0x10
+    jmp wait_key
+
+clean_screen:
+    mov ah, 0x06        ; Función 06h de INT 10h - Desplazar pantalla
+    mov al, 0x00        ; Atributo de fondo de pantalla (0x00 = blanco)
+    mov bh, 0x00        ; Página de video
+    mov cx, 0x0000      ; Esquina superior izquierda
+    mov dx, 0x184f      ; Esquina inferior derecha (80 columnas x 25 filas)
+    int 0x10            ; Llamada a la BIOS
+
 row:
     db 0
 
@@ -61,7 +111,7 @@ dw 0xAA55
 ; nasm boot.asm 
 ; qemu-system-i386 boot
 
-;or
+; or
 
 ; nasm -f bin boot.asm -o boot.bin
 ; qemu-system-i386 -drive format=raw,file=boot.bin
