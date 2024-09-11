53c818c2306496977823912f04055e08e7f5b77d
valeriam07 <47421582+valeriam07@users.noreply.github.com>
2024-09-11 14:17:34 -0600

    Movement right and left

--- boot.asm
+++ boot.asm
@@ -1,11 +1,10 @@
 bits 16
 org 0x7c00
 
-
 _start:
     mov ah, 0x00        ; Function 00h de INT 1Ah -set video mode
     mov al, 0x13        ; grafic mode 13h (320x200 pix, 256 colors)
-    int 0x10            ; call a la BIOS 
+    int 0x10            ; call a la BIOS
     ;call draw_welcome
     call random
     call wait_key
@@ -23,8 +22,8 @@ draw_pix:
         mov bp, 8                ; 8 columns, 1 byte
 
     next_pixel:
-        test di, 80h             
-        jz skip_pixel           
+        test di, 80h            
+        jz skip_pixel          
 
         ; Draw pix (dx, cx)
         mov ah, 0x0C            ; call Bios
@@ -33,16 +32,48 @@ draw_pix:
         int 0x10                
 
     skip_pixel:
-        shl di, 1               
+        shl di, 1              
         inc dx                  
         dec bp                  
         jnz next_pixel          
         ; next row
         mov dx, [column]        
-        inc cx 
+        inc cx
         inc si                  
         dec bx                  
         jnz next_row  
+        ret
+
+draw_pix_vertical:
+    mov bx, 48                ; Numero total de filas
+    mov cx, [row]             ; Posición Y inicial
+    mov dx, [column]          ; Posición X inicial
+
+    next_row_vertical:
+        mov di, [si]          
+        mov bp, 8            
+
+    next_pixel_vertical:
+        test di, 80h          
+        jz skip_pixel_vertical
+
+        mov ah, 0x0C          
+        mov al, 0x0F          ; Color (blanco)
+        mov bh, 0x00          ; Pagina de video
+        int 0x10              ; Llamada a la BIOS
+
+    skip_pixel_vertical:
+        shl di, 1             ; Mover el byte a la izquierda (siguiente bit)
+        inc cx                ; Incrementar la posicion Y (bajar un píxel)
+        dec bp                ; Decrementar el contador de columnas
+        jnz next_pixel_vertical
+
+        ; Pasar a la siguiente columna
+        mov cx, [row]         ; Reiniciar la posicion Y a la fila inicial
+        inc dx                ; Incrementar la posicion X (mover a la derecha)
+        inc si                ; Cargar el siguiente byte de la letra
+        dec bx                ; Decrementar el numero de filas restantes
+        jnz next_row_vertical
         ret
 
 ;random rows & columns
@@ -57,11 +88,11 @@ random:
     div cx              ; Divide AX por 25,
     mov [row], dx  
 
-    mov ax, dx         
+    mov ax, dx        
     xor dx, dx          
     mov cx, 320          ; max num columns (0-319)
     div cx              
-    mov [column], dx   
+    mov [column], dx  
     ret
 
 clean_screen:
@@ -78,7 +109,12 @@ wait_key:
     je initial_name_key
     cmp al, 'k'
     je name_down_key
-    jmp wait_key    
+    cmp al, 'j'
+    je name_left_key
+    cmp al, 'l'
+    je name_right_key
+    jmp wait_key
+   
 
     initial_name_key:
         int 0x10                   ; call BIOS
@@ -90,69 +126,84 @@ wait_key:
     name_down_key:
         int 0x10        
         call clean_screen
-        mov si, name_down     
+        mov si, name_down    
         call draw_pix
-        jmp wait_key    
+        jmp wait_key
+   
+    name_left_key:
+        int 0x10        
+        call clean_screen
+        mov si, name_down    
+        call draw_pix_vertical
+        jmp wait_key
+       
+    name_right_key:
+        int 0x10        
+        call clean_screen
+        mov si, name_right
+        call draw_pix_vertical
+        jmp wait_key
+     
 
 initial_name:
-    db 0b1000000 
+    db 0b1000000
     db 0b1000000
     db 0b1111111
-    db 0b1000000 
-    db 0b1000000 
+    db 0b1000000
+    db 0b1000000
     db 0b0000000
 
     db 0b0011111  
     db 0b0010101  
     db 0b0010101  
     db 0b0011101  
-    db 0b0000000 
+    db 0b0000000
 
-    db 0b1111111 
+    db 0b1111111
     db 0b1001000
     db 0b1001000
-    db 0b1000000 
-    db 0b0000000 
+    db 0b1000000
+    db 0b0000000
 
-    db 0b0000011 
+    db 0b0000011
     db 0b0001100
     db 0b1110100
-    db 0b1000100 
-    db 0b1000100 
+    db 0b1000100
+    db 0b1000100
     db 0b1110100
     db 0b0001100
     db 0b0000011
-    
-    db 0b1100000 
+   
+    db 0b1100000
     db 0b0011000
     db 0b0000110
-    db 0b0000001 
-    db 0b0000110 
+    db 0b0000001
+    db 0b0000110
     db 0b0011000
     db 0b1100000
     db 0b0000000
-    
-    db 0b0000011 
+   
+    db 0b0000011
     db 0b0001100
     db 0b1110100
-    db 0b1000100 
-    db 0b1000100 
+    db 0b1000100
+    db 0b1000100
     db 0b1110100
     db 0b0001100
     db 0b0000011
-    
-    db 0b1111111 
+   
+    db 0b1111111
     db 0b0000001
     db 0b0000001
-    db 0b0000001 
-    db 0b0000000 
-    
+    db 0b0000001
+    db 0b0000000
+   
     db 0b0011111  
     db 0b0010101  
     db 0b0010101  
     db 0b0011101  
-    db 0b0000000 
-    
+    db 0b0000000
+   
 
 
 name_down:
@@ -184,14 +235,14 @@ name_down:
     db 0b0011000
     db 0b1100000
 
-    db 0b0000011 
+    db 0b0000011
     db 0b0000110
     db 0b0110000
     db 0b1000000
     db 0b0110000
     db 0b0000110
     db 0b0000011
-    db 0b0000000 
+    db 0b0000000
 
     db 0b1100000
     db 0b0011000
@@ -202,17 +253,79 @@ name_down:
     db 0b0011000
     db 0b1100000
 
-    db 0b1111111 
+    db 0b1111111
     db 0b1000000
     db 0b1000000
     db 0b1000000
-    db 0b0000000 
+    db 0b0000000
 
     db 0b11110000
     db 0b10101000
     db 0b10101000
     db 0b10111000
     db 0b00000000
+   
+name_right:
+    db 0b0011111  
+    db 0b0010101  
+    db 0b0010101  
+    db 0b0011101  
+    db 0b0000000
+   
+    db 0b1111111
+    db 0b0000001
+    db 0b0000001
+    db 0b0000001
+    db 0b0000000
+
+    db 0b0000011
+    db 0b0001100
+    db 0b1110100
+    db 0b1000100
+    db 0b1000100
+    db 0b1110100
+    db 0b0001100
+    db 0b0000011
+   
+    db 0b1100000
+    db 0b0011000
+    db 0b0000110
+    db 0b0000001
+    db 0b0000110
+    db 0b0011000
+    db 0b1100000
+    db 0b0000000
+
+    db 0b0000011
+    db 0b0001100
+    db 0b1110100
+    db 0b1000100
+    db 0b1000100
+    db 0b1110100
+    db 0b0001100
+    db 0b0000011
+
+    db 0b1111111
+    db 0b1001000
+    db 0b1001000
+    db 0b1000000
+    db 0b0000000
+
+    db 0b0011111  
+    db 0b0010101  
+    db 0b0010101  
+    db 0b0011101  
+    db 0b0000000
+   
+    db 0b1000000
+    db 0b1000000
+    db 0b1111111
+    db 0b1000000
+    db 0b1000000
+    db 0b0000000
+
+
+
 
 
 initial_screen:
@@ -233,10 +346,10 @@ row:
 column:
     db 0
 
-times 510 - ($ - $$) db 0 
+times 510 - ($ - $$) db 0
 dw 0xAA55              
 
-; nasm boot.asm 
+; nasm boot.asm
 ; qemu-system-i386 boot
 
 ; or
